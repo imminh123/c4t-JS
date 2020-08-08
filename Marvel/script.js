3f5ffdc4f03ab8a28a25d4c6b62d9fcbd544419b
Minh Nguyen <minhco12@gmail.comm>
2020-08-08 19:18:05 +0700

    first commit

--- Marvel/script.js
+++ Marvel/script.js
@@ -4,7 +4,6 @@ let apiURL = 'https://gateway.marvel.com:443/v1/public/characters';
 
 //generate key
 let key = marvelKey(privateKey, publicKey);
-
 let fullURL = `${apiURL}?${key}`;
 
 async function getCharacters() {
@@ -12,7 +11,27 @@ async function getCharacters() {
   let dataJSON = await data.json();
 
   let results = dataJSON.data.results;
+  renderData(results);
+}
 
+getCharacters();
+
+
+//search heros
+$('#searchBtn').on('click', async function() {
+  let inputValue = $('#searchBar').val();
+  let searchUrl = `${apiURL}?${key}&nameStartsWith=${inputValue}`;
+
+  let data = await fetch(searchUrl);
+  let searchHero = await data.json();
+
+  let finalData = searchHero.data.results;
+
+  renderData(finalData);
+})
+
+//renderData
+function renderData(results) {
   let renderData = "";
 
   for(let i = 0; i < results.length; i++) {
@@ -20,16 +39,17 @@ async function getCharacters() {
     let path = results[i].thumbnail.path;
     let extension = results[i].thumbnail.extension;
     let available = results[i].comics.available;
+    let id = results[i].id;
 
     renderData += `
-    <div class="character">
-      <img src="${ path + '.' + extension }" alt="">
-      <h5>${name}</h5>
-      <h6>Comics : ${available}</h6>
-    </div>
+      <div class="character">
+        <a href="/Marvel/detail.html?id=${id}">
+          <img src="${ path + '.' + extension }" alt="">
+              <h5>${name}</h5>
+          <h6>Comics : ${available}</h6>
+        </a>
+      </div>
     `
-    $('#root').html(renderData);
+    $('#content').html(renderData);
   }
 }
-
-getCharacters();
